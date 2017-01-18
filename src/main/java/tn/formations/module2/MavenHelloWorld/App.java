c4497604a2fa85061c853279dd55e90032ec7d12
Jared Thomas <jjthom87@yahoo.com>
2017-01-18 07:56:29 -0500

    first push

---
+++ src/main/java/tn/formations/module2/MavenHelloWorld/App.java
@@ -0,0 +1,13 @@
+package tn.formations.module2.MavenHelloWorld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "this is pretty dope" );
+    }
+}
